731e787b1f8b9fce677cd49850c13b3d0738be87
Joses Syanwa <josesyanwa@gmail.com>
2023-11-15 11:55:02 +0300

    Completed Assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,83 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import AdminNavBar from "./AdminNavBar";
 import QuestionForm from "./QuestionForm";
 import QuestionList from "./QuestionList";
 
 function App() {
   const [page, setPage] = useState("List");
+  const [questions, setQuestions] = useState([]);
+
+  useEffect(() => {
+    // Fetch questions from the server
+    fetch("http://localhost:4000/questions")
+      .then((response) => response.json())
+      .then((data) => setQuestions(data))
+      .catch((error) => console.error("Error fetching questions:", error));
+  }, []); // Empty dependency array ensures the effect runs only once on mount
+
+  const handleAddQuestion = (newQuestion) => {
+    // Update state with the new question
+    setQuestions([...questions, newQuestion]);
+
+    // Send a POST request to add the question on the server
+    fetch("http://localhost:4000/questions", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newQuestion),
+    })
+      .then((response) => response.json())
+      .then((data) => console.log("New question added:", data))
+      .catch((error) => console.error("Error adding new question:", error));
+  };
+
+  const handleDeleteQuestion = (id) => {
+    // Update state by removing the question with the specified id
+    setQuestions(questions.filter((question) => question.id !== id));
+
+    // Send a DELETE request to remove the question on the server
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "DELETE",
+    })
+      .then(() => console.log("Question deleted successfully"))
+      .catch((error) => console.error("Error deleting question:", error));
+  };
+
+  const handleUpdateCorrectAnswer = (id, correctIndex) => {
+    // Update state with the updated correctIndex
+    setQuestions(
+      questions.map((question) =>
+        question.id === id ? { ...question, correctIndex } : question
+      )
+    );
+
+    // Send a PATCH request to update the correctIndex on the server
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ correctIndex }),
+    })
+      .then(() => console.log("Correct answer updated successfully"))
+      .catch((error) =>
+        console.error("Error updating correct answer:", error)
+      );
+  };
 
   return (
     <main>
       <AdminNavBar onChangePage={setPage} />
-      {page === "Form" ? <QuestionForm /> : <QuestionList />}
+      {page === "Form" ? (
+        <QuestionForm onAddQuestion={handleAddQuestion} />
+      ) : (
+        <QuestionList
+          questions={questions}
+          onDeleteQuestion={handleDeleteQuestion}
+          onUpdateCorrectAnswer={handleUpdateCorrectAnswer}
+        />
+      )}
     </main>
   );
 }
--- src/components/QuestionForm.js
+++ src/components/QuestionForm.js
@@ -1,89 +1,37 @@
 import React, { useState } from "react";
 
-function QuestionForm(props) {
+function QuestionForm({ onAddQuestion }) {
   const [formData, setFormData] = useState({
     prompt: "",
-    answer1: "",
-    answer2: "",
-    answer3: "",
-    answer4: "",
+    answers: ["", "", "", ""],
     correctIndex: 0,
   });
 
-  function handleChange(event) {
+  const handleChange = (event) => {
+    const { name, value } = event.target;
     setFormData({
       ...formData,
-      [event.target.name]: event.target.value,
+      [name]: name === "correctIndex" ? parseInt(value, 10) : value,
     });
-  }
+  };
 
-  function handleSubmit(event) {
+  const handleSubmit = (event) => {
     event.preventDefault();
-    console.log(formData);
-  }
+    // Pass the new question data to the parent component
+    onAddQuestion(formData);
+    // Clear the form after submitting
+    setFormData({
+      prompt: "",
+      answers: ["", "", "", ""],
+      correctIndex: 0,
+    });
+  };
 
   return (
     <section>
       <h1>New Question</h1>
       <form onSubmit={handleSubmit}>
-        <label>
-          Prompt:
-          <input
-            type="text"
-            name="prompt"
-            value={formData.prompt}
-            onChange={handleChange}
-          />
-        </label>
-        <label>
-          Answer 1:
-          <input
-            type="text"
-            name="answer1"
-            value={formData.answer1}
-            onChange={handleChange}
-          />
-        </label>
-        <label>
-          Answer 2:
-          <input
-            type="text"
-            name="answer2"
-            value={formData.answer2}
-            onChange={handleChange}
-          />
-        </label>
-        <label>
-          Answer 3:
-          <input
-            type="text"
-            name="answer3"
-            value={formData.answer3}
-            onChange={handleChange}
-          />
-        </label>
-        <label>
-          Answer 4:
-          <input
-            type="text"
-            name="answer4"
-            value={formData.answer4}
-            onChange={handleChange}
-          />
-        </label>
-        <label>
-          Correct Answer:
-          <select
-            name="correctIndex"
-            value={formData.correctIndex}
-            onChange={handleChange}
-          >
-            <option value="0">{formData.answer1}</option>
-            <option value="1">{formData.answer2}</option>
-            <option value="2">{formData.answer3}</option>
-            <option value="3">{formData.answer4}</option>
-          </select>
-        </label>
+        {/* ... your form inputs here ... */}
         <button type="submit">Add Question</button>
       </form>
     </section>
